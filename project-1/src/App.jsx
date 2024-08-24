f6c1f473afd40bbb3c4d1028175047265827dc4e
Dwaipayan <biswastatay73@gmail.com>
2024-08-24 06:16:11 +0530

    update

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -3,8 +3,16 @@ const App = () => {
     <div>
       <nav>
         <div className='logo'>
-          <img src='/images/brand_logo.png'></img>
+          <img src='/images/brand_logo.png' alt='logo'></img>
         </div>
+        <ul>
+          <li href='#'>Menu</li>
+          <li href='#'>Location</li>
+          <li href='#'>About</li>
+          <li href='#'>Contact</li>
+        </ul>
+
+        <button>Login</button>
       </nav>
     </div>
   );
