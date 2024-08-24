98220c10b8a301c6671388bb9d608f135e0f772e
Dwaipayan <biswastatay73@gmail.com>
2024-08-24 05:50:55 +0530

    New Project

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <div className='logo'>
+          <img src='/images/brand_logo.png'></img>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
